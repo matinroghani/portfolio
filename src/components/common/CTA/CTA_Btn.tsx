78cb02779a619d88cacdb56e5203c9cc4d94a990
roghani-matin <martin.pars.sh@gmail.com>
2026-08-17 14:42:44 +0330

    Update Project - Debug Build Error

--- src/components/common/CTA/CTA_Btn.tsx
+++ src/components/common/CTA/CTA_Btn.tsx
@@ -6,7 +6,7 @@ export default function CTA_Btn() {
   return (
     <div className="flex items-center gap-7">
 
-      <Cta_Primrary title="View My Work" icon={<ArrowRight  size={20} strokeWidth={1.8} />}/>
+      <Cta_Primrary title="View My Work" pastIcon={<ArrowRight  size={20} strokeWidth={1.8} />}/>
 
       <Cta_Secondery title="Get In Touch" icon={<Mail size={20} strokeWidth={1.8} />} />
 
